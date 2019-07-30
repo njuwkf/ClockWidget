81822f490130fb8de3b508b9a56f7d801901af32
吴科烽 <i_wukefeng@cvte.com>
2019-07-30 16:42:58 +0800

    first commit

--- app/src/main/java/com/example/clockwidget/ClockProvider.java
+++ app/src/main/java/com/example/clockwidget/ClockProvider.java
@@ -4,34 +4,38 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.RemoteViews;
 
 public class ClockProvider extends AppWidgetProvider {
-    //刷新执行
+
+    // 刷新的时候执行
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
-        super.onUpdate(context,appWidgetManager,appWidgetIds);
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        // TODO Auto-generated method stub
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
-    //第一个添加到屏幕上
+    // 第一个添加到屏幕上
     @Override
     public void onEnabled(Context context) {
+        // TODO Auto-generated method stub
         super.onEnabled(context);
-        context.startService(new Intent(context,ClockService.class));
+        // 启动Service
+        context.startService(new Intent(context, ClockService.class));
     }
 
-    //最后一个widget从屏幕移除
+    // 最后一个widget从屏幕移除
     @Override
     public void onDisabled(Context context) {
+        // TODO Auto-generated method stub
         super.onDisabled(context);
-        context.stopService(new Intent(context,ClockService.class));
+        context.stopService(new Intent(context, ClockService.class));
     }
 
-    //从屏幕移除
-
+    // 从屏幕移除
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
+        // TODO Auto-generated method stub
         super.onDeleted(context, appWidgetIds);
     }
 }
--- app/src/main/java/com/example/clockwidget/ClockService.java
+++ app/src/main/java/com/example/clockwidget/ClockService.java
@@ -5,17 +5,21 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ClockService extends Service {
     private Timer timer;
-    private SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
+    private SimpleDateFormat simpleDateFormat=new SimpleDateFormat("HH:mm:ss");
+    private String[] week={"周一","周二","周三","周四","周五","周六","周日"};
     @Override
     public IBinder onBind(Intent intent){
         return null;
@@ -35,11 +39,14 @@ public class ClockService extends Service {
 
     private void updateView(){
         //时间
-        String time = sdf.format(new Date());
+        String str_time = simpleDateFormat.format(new Date());
+        String str_date=getDateString()+"  "+getDateInWeek();
         RemoteViews rViews = new RemoteViews(getPackageName(),R.layout.clockwidge_activity);
         //显示当前时间
-        rViews.setTextViewText(R.id.id_time,time);
-
+        // Log.e("time",str_time);
+      //  Log.e("date",str_date);
+        rViews.setTextViewText(R.id.clock_time,str_time);
+        rViews.setTextViewText(R.id.date_time,str_date);
         //刷新
         AppWidgetManager manager = AppWidgetManager.getInstance(getApplicationContext());
         ComponentName cName = new ComponentName(getApplicationContext(),ClockProvider.class);
@@ -50,5 +57,34 @@ public class ClockService extends Service {
     public void onDestroy() {
         super.onDestroy();
         timer = null;
+    }
+
+    /**
+     * 获得周几
+     * @param
+     * @return
+     */
+    private String getDateInWeek() {
+        SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy-MM");
+        String[] mweekdays = { "周日", "周一", "周二", "周三", "周四", "周五", "周六" };
+        Calendar mCalendar = Calendar.getInstance();
+        Date mDate=new Date();
+        mCalendar.setTime(mDate);
+        int weekday = mCalendar.get(Calendar.DAY_OF_WEEK) - 1; // 指示一个星期中的某天。
+        if (weekday < 0)
+            weekday = 0;
+        return mweekdays[weekday];
+    }
+    /**
+     * 获得mm月dd日类型的日期
+     * @param
+     * @return
+     */
+    private String getDateString(){
+        String[] mstr_now=new SimpleDateFormat("yyyy-MM-dd").format(new Date()).toString().split("-");
+        String mstr_month = mstr_now[1].replaceAll("^(0+)", "");
+        String mstr_day = mstr_now[2].replaceAll("^(0+)", "");
+        String mstr_date=mstr_month+"月"+mstr_day+"日";
+        return mstr_date;
     }
 }
