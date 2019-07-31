70e51eddebf5881b2b86ed5480365ca57866bda6
吴科烽 <i_wukefeng@cvte.com>
2019-07-31 11:31:10 +0800

    timesetting first commit

--- app/src/main/java/com/example/clockwidget/DateSettingsActivity.java
+++ app/src/main/java/com/example/clockwidget/DateSettingsActivity.java
@@ -1,4 +1,43 @@
 package com.example.clockwidget;
 
-public class DateSettingsActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class DateSettingsActivity extends Activity implements View.OnClickListener {
+    private LinearLayout mdate_linearLayout;
+    private LinearLayout mtime_linearLayout;
+    private TextView mdate_text;
+    private TextView mtime_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datesettings_activity);
+        mdate_linearLayout = findViewById(R.id.ll_date);
+        mtime_linearLayout = findViewById(R.id.ll_time);
+        mdate_text=findViewById(R.id.tv_selected_date);
+        mtime_text=findViewById(R.id.tv_selected_time);
+        mdate_linearLayout.setOnClickListener(this);
+        mtime_linearLayout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_date:
+                DateUtil.showDatePickerDialog(DateSettingsActivity.this,2, mdate_text);
+                break;
+            case R.id.ll_time:
+                DateUtil.showTimePickerDialog(DateSettingsActivity.this,2, mtime_text);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/clockwidget/DateUtil.java
@@ -0,0 +1,63 @@
+package com.example.clockwidget;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class DateUtil {
+    /**
+     * 日期选择
+     *
+     * @param activity
+     * @param themeResId
+     * @param tv
+     */
+    public static void showDatePickerDialog(Activity activity, int themeResId, final TextView tv){
+        Calendar mCalendar = Calendar.getInstance();
+        // 直接创建一个DatePickerDialog对话框实例，并将它显示出来
+        new DatePickerDialog(activity, themeResId,
+                // 绑定监听器
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        tv.setText(year + "." + (monthOfYear + 1) + "." + dayOfMonth );
+                    }
+                }
+                // 设置初始日期
+                ,mCalendar.get(Calendar.YEAR)
+                , mCalendar.get(Calendar.MONTH)
+                , mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    /**
+     * 时间选择
+     *
+     * @param activity
+     * @param themeResId
+     * @param tv
+     */
+    public static void showTimePickerDialog(Activity activity, int themeResId, final TextView tv) {
+        Calendar mCalendar = Calendar.getInstance();
+        // 创建一个TimePickerDialog实例，并把它显示出来
+        // 解释一哈，Activity是context的子类
+        new TimePickerDialog(activity, themeResId,
+                // 绑定监听器
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        tv.setText(hourOfDay + ":" + minute );
+                    }
+                }
+                // 设置初始时间
+                , mCalendar.get(Calendar.HOUR_OF_DAY)
+                , mCalendar.get(Calendar.MINUTE)
+                // true表示采用24小时制
+                , true).show();
+    }
+}
